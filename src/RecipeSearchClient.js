a76f179dbd9cc73c46c9a16b28b83cb4765d1533
Cameron Wilby <cameron@wilby.com>
2019-08-09 18:59:54 +0100

    Fix recipe search client

--- src/RecipeSearchClient.js
+++ src/RecipeSearchClient.js
@@ -6,40 +6,44 @@ module.exports = class RecipeSearchClient extends EdamamClient {
   }
 
   search({
-    query,
-    recipeId,
+    query = null,
+    recipeId = null,
     limit = {
       from: 0,
       to: 10
     },
-    maxIngredients,
-    dietLabel,
-    healthLabel,
-    cuisineType,
-    mealType,
-    dishType,
-    calories,
-    time,
-    excluded
+    maxIngredients = null,
+    mealType = null,
+    calories = null,
+    time = null,
+    dietLabels = [],
+    healthLabels = [],
+    cuisineTypeLabels = [],
+    dishTypeLabels = [],
+    excludedIngredients = []
   }) {
-    return this.get('*search', {
+    const params = [
+      ['q', query],
+      ['r', recipeId],
+      ['from', limit.from],
+      ['to', limit.to],
+      ['ingr', maxIngredients],
+      ['mealType', mealType],
+      ['calories', calories],
+      ['time', time],
+      ...dietLabels.map(dl => ['diet', dl]),
+      ...healthLabels.map(hl => ['health', hl]),
+      ...cuisineTypeLabels.map(ctl => ['cuisineType', ctl]),
+      ...dishTypeLabels.map(dtl => ['dishType', dtl]),
+      ...excludedIngredients.map(ei => ['excluded', ei])
+    ]
+      .map(([key, value]) => [null, undefined].includes(value) ? null : `${key}=${encodeURIComponent(value)}`)
+      .filter(x => x)
+      .join('&');
+
+    return this.get(`*search?${params}`, {
       headers: {
         'Accept-Encoding': 'gzip'
-      },
-      params: {
-        q: query,
-        r: recipeId,
-        from: limit.from,
-        to: limit.to,
-        ingr: maxIngredients,
-        diet: dietLabel,
-        health: healthLabel,
-        cuisineType,
-        mealType,
-        dishType,
-        calories,
-        time,
-        excluded
       }
     });
   }
